150e89c64bd8a72f43fdf10b291bac8938a1b611
Zach Dimond <zedimond@gmail.com>
2022-08-24 13:23:04 -0600

    duplicated card data X3 so there are more cards and the user can more clearly grasp the function of the cards area

--- src/data.js
+++ src/data.js
@@ -38,5 +38,77 @@ export default [
         price: "$136",
         priceUnit: "biker",
         img: bike
-    }
+    },
+    {
+        id: 1,
+        title: "Life lessons with Katie Zaferes",
+        stats: {
+            rating: "5.0",
+            reviewCount: "6"
+        },
+        country: "USA",
+        price: "$136",
+        priceUnit: "person",
+        img: zaferes
+    },
+    {
+        id: 2,
+        title: "Learn wedding photography",
+        stats: {
+            rating: "5.0",
+            reviewCount: "30"
+        },
+        country: "USA",
+        price: "$125",
+        priceUnit: "couple",
+        img: wedding
+    },
+    {
+        id: 3,
+        title: "Group mountain biking",
+        stats: {
+            rating: "4.8",
+            reviewCount: "2"
+        },
+        country: "USA",
+        price: "$136",
+        priceUnit: "biker",
+        img: bike
+    },
+    {
+        id: 1,
+        title: "Life lessons with Katie Zaferes",
+        stats: {
+            rating: "5.0",
+            reviewCount: "6"
+        },
+        country: "USA",
+        price: "$136",
+        priceUnit: "person",
+        img: zaferes
+    },
+    {
+        id: 2,
+        title: "Learn wedding photography",
+        stats: {
+            rating: "5.0",
+            reviewCount: "30"
+        },
+        country: "USA",
+        price: "$125",
+        priceUnit: "couple",
+        img: wedding
+    },
+    {
+        id: 3,
+        title: "Group mountain biking",
+        stats: {
+            rating: "4.8",
+            reviewCount: "2"
+        },
+        country: "USA",
+        price: "$136",
+        priceUnit: "biker",
+        img: bike
+    },
 ]
